db757ddf33a00fcf90edac743a7b64eded98b0cf
msrai92 <m.singh.rai15@gmail.com>
2020-03-02 21:13:27 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// connect db 
+connectDB();
+
+app.get('/', (req,res) => res.send('API RUNNING'));
+
+// define routes 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
